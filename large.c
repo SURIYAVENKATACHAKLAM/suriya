112b23920427fe556fa8d13a7d2377ec93e8de92
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-01-19 10:58:06 +0000

    Create large.c

---
+++ large.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    int largest;
+    printf("enter three number");
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b && a>c)
+    largest=a;
+    else if(b>a && b>c)
+    largest=b;
+    else
+    largest=c;
+    printf("largest number is=%d",largest);
+    return 0;
+    
+}
