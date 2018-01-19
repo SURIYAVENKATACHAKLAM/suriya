baecf809421ed9d3c3b5983d59eeb987909f21d2
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-01-19 10:55:40 +0000

    Create ss.c

---
+++ ss.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a=0;
+   // printf("enter the value a");
+    //scanf("%d",&a);
+    if(a>0)
+    printf("%d is a positive number",a);
+    else
+    printf("%d it is not a positive number",a);
+    
+}
