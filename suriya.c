7d3366ab78c3e5c53b29c473e8e90670d5b5a09e
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-01-19 11:07:14 +0000

    Create suriya.c

---
+++ suriya.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,sum=0;
+    printf("enter the value");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    sum =sum+i;
+    printf("sum of the number%d",sum);
+    return 0;
+    
+}
