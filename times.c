08a5e734046bbd1eaba39ab15113047e11c21b2e
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-01-19 11:01:22 +0000

    Create times.c

---
+++ times.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{   
+    int b,i;
+    char a[100]="hello";
+    printf("enter the times");
+    scanf("%d",&b);
+    for(i>=0;i<=b;i++)
+    {
+    printf("%s",a);
+    }
+}
+
