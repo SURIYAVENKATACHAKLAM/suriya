32c2150b23947ee35048795cc5fb02400b27694d
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-01-21 15:09:52 +0530

    Create sakathi.c

---
+++ sakathi.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int Fibonacci(int); 
+int main()
+{
+int n, i = 0, c; 
+scanf("%d",&n); 
+printf("Fibonacci series\n"); 
+for ( c = 1 ; c <= n ; c++ )
+{
+printf("%d\n", Fibonacci(i));
+i++; 
+}
+return 0;
+getch();
+}
