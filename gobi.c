e382cc9ee5b13203ba710f4069ad2c85b3c76f6e
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-01-21 15:41:39 +0530

    Create gobi.c

---
+++ gobi.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int count=0,a;
+printf("enter the number");
+scanf("%d",&a);
+while(a!=0)
+{
+a=a/10;
+count++;
+printf(" count the number%d",count);
+return 0;
+}
+getch();
+}
