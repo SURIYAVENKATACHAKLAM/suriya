4f879abbfc53e27231ec1b2b759685fadc80bd98
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-01-21 14:30:50 +0530

    Create rs.c

---
+++ rs.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a;
+char ch;
+scanf("%d",&a);
+if((ch>=65&&ch<=90)||(ch>=97&&ch<=122))
+{
+printf("%d not a number",&a);
+}
+else
+{
+printf("%d number",&a);
+}
+return0;
+}
