d4b3d85d447defc9f7dab9634b6ad83be5935624
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-01-21 15:13:43 +0530

    Create raj.c

---
+++ raj.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+
+       char s1[5],s2[5];
+       int i,temp = 0;
+
+       printf("Enter the string1 value: ");
+       gets(s1);
+
+       printf(" Enter the String2 value: ");
+       gets(s2);
+
+       for(i=0; s1[i]!='\0'; i++)
+       {
+      if(s[i] == s2[i])
+      temp = 1;
+       else
+       temp = 0;
+       }
+       if(temp == 1)
+       printf("Both strings are same.");
+       else
+       printf("Both string not same.");
+       return 0;
+       }
