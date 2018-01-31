2b3496d4842ad4631c436e52795267130d3c27fd
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-01-31 16:07:31 +0530

    Create tamilan.c

---
+++ tamilan.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int num,n;
+    printf("enter the number");
+    scanf("%d",&num);
+    while(number%10!=0)
+    {
+        numb++;
+    }
+printf("nearest greater multiple of 10 %d",num);
+    }
+    return 0;
