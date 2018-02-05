f577eefe6f0c358bb81afc9a5f7b773d0b867d1e
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-02-05 15:54:14 +0530

    Create yes no.c

---
+++ yes no.c
@@ -0,0 +1,17 @@
+#include<stdio.>
+#include<string.h>
+void main()
+{
+char c;
+printf("enter the character");
+scanf("%c",&c);
+if(c=='a'|| c=='A'|| c=='e'|| c=='E' || c=='i' ||c=='I' || c=='o' || c=='O' ||c=='u' ||c=='U')
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
