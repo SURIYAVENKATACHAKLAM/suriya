a6b14377c9c1c033b4c58dddeca9745083eb9a48
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-02-05 16:17:07 +0530

    Create l.c

---
+++ l.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int a,l;
+l=5;
+printf("enter the number");
+scanf("%d",&a);
+if(l<a)
+{
+printf("enter:yes");
+}
+else
+{
+printf("enter:no");
+}
+return 0;
+}
