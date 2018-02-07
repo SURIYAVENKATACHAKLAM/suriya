c0eb481ad7e88a31658c9b64b5265f734531df11
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-02-07 16:10:06 +0530

    Create 81.c

---
+++ 81.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int kabali=0,ninja,diff;
+printf("\nEnter the Number of Opponents :);
+scanf("%d",&ninja);
+for(i=0;i<ninja;i++)
+{
+kabali=kabali+i;
+}
+printf("%d",kabali);
+diff=ninja-kabali;
+if(diff>0)
+{
+printf("Kabali should not go for Fight");
+}
+else
+{
+printf("Kabali can go for Fight");
+}
+return 0;
+}
