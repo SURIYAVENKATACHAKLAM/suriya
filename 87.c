7b3fba648e01d0b6b2bc8079ba16d819ed0b544e
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-02-09 14:22:58 +0530

    Create 87.c

---
+++ 87.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+int a,b,i,gcd;
+printf("enter two number");
+scanf("%d%d",&a,&b);
+for(i=1;i<=a&&i<=b;++i)
+{
+if((a%i==0) && (b%i==0))
+{
+gcd=i;
+}
+printf("%d",gcd);
+}
