09089e23c736acd4bedb847bd49d9165ebf341ed
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-02-12 16:31:45 +0530

    Rename 83.c to q8120.c

---
+++ q8120.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void main()
+{
+	char str[20];
+	int i,n;
+	printf("nenter the string:\n");
+	scanf("%s",str);
+	n=strlen(str);
+	for(i=0;i<n;i++)
+	{
+		if(i%2==0)
+		{
+			printf("%c",str[i]);
+		}
+		
+	}
+	printf(" ");
+
+
