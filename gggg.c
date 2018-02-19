59f70db63e43a84376db4ee8a522f240609d6cfc
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-02-19 16:39:14 +0530

    Create gggg.c

---
+++ gggg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n,k,a[10],i;
+printf("enter the number of element");
+scanf("%d%d",&n,&k);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&n[10]);
+printf("enter the value %d ");
+scanf("%d",&k[10]);
+printf("enter the kth elemant");
+}
+}
+
