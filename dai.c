3be75e43d9a4c3230ceecd23d343cb3181806992
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-02-26 02:44:43 -0800

    Create dai.c

---
+++ dai.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int a[10],i,max,n;
+    printf("enter the number");
+    scanf("%d",&n);
+    printf("enter the n value");
+    for(i=0;i<n;i++)
+    {
+    scanf("%d",&a
+        }
+    
+    }
+    for(i=0;i<n;i++)
+    {
+        if(max==a[i])
+    printf("%d",i+1);
+    }
+    return 0;
+}
