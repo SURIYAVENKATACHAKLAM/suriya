411ad9f75e4824bbd3558db998819eaf70a08661
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-03-02 15:42:32 +0530

    Rename 106.c to 106ammu.c

---
+++ 106ammu.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main(void) {
+	int a,b,c,ans;
+	printf("enter the values of a,b,c\n");
+	scanf("%d%d%d",&a,&b,&c);
+	ans=(a*b)/c;
+	printf("%d",ans);
+	
+}
