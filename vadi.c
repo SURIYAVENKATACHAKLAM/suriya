380066af1bd94f5e3011fbcd3185f776b17fb4c8
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-03-12 16:53:08 +0530

    Create vadi.c

---
+++ vadi.c
@@ -0,0 +1,28 @@
+main()
+{
+    int i, n;
+    int n[100];
+    float k[100];
+    cout<<"Enter total number of elements in n";
+
+    cout << "Enter total number of elements in k";
+    cin >> n;
+    cout << endl;
+    if(n[i]>k[i])
+   {
+    for(i = 0; i < n; ++i)
+    {
+       cout << "Enter Number " << i + 1 << " : ";
+       cin >> k[i];
+    }
+
+    // Loop to store largest number to arr[0]
+    for(i = 1;i < n; ++i)
+    {
+          if(k[0] < k[i])
+           k[0] = k[i];
+    }
+    cout << "Largest element = " << k[0];
+}
+    
+}
