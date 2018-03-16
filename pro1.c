61075d6d3540329b5ea2df7f929d4cf17222ddb6
SURIYAVENKATACHAKLAM <35220735+SURIYAVENKATACHAKLAM@users.noreply.github.com>
2018-03-16 16:55:24 +0530

    Create pro1.c

---
+++ pro1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+char day;
+printf("enter the string");
+scanf("%d",&day);
+switch(day)
+{
+case monday
+printf("no");
+break;:
+case tuesday
+printf("no");
+break:
+case wenesday
+printf("no");
+break:
+case thursday
+printf("no");
+break:
+case friday
+printf("no");
+break:
+case saturday
+printf("yes");
+break:
+case sunday
+printf("yes");
+break:
+}
